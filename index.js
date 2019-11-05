ae33c79c83a56a85893cb73da27785e6ecfdfa2f
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2019-11-04 22:42:45 -0500

    ssl

--- index.js
+++ index.js
@@ -25,9 +25,10 @@ const proxy = httpProxy.createProxyServer({ ssl });
 const server = http.createServer( function(req, res) {
   // You can define here your custom logic to handle the request
   // and then proxy the request.gi
+  console.log(req.headers);
+
   let hostName = req.headers.host.split(':')[0];
   let port = req.headers.host.split(':')[1];
-  console.log(req.headers);
   if (hostName === 'explorer.blockchainfoundry.co') {
     if (port !== config.wsPort) {
       console.log(`Routing to explorer port ${config.explorer.http}`);
