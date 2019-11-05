94c870898f40c63c49848ccc7120d281fcbf663c
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2019-11-05 00:39:29 -0500

    ws

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const server = https.createServer( ssl, function(req, res) {
   if (port) port = +port; //convert to number
   if (host === 'explorer.blockchainfoundry.co') {
     console.log(`Routing to explorer port ${config.explorer.http}`);
-    proxy.web(req, res, {target: {host: 'localhost', port: 9999}});
+    proxy.web(req, res, {target: {host: 'localhost', port: 9999}, ws: true});
   } else if (host === 'explorer-testnet.blockchainfoundry.co') {
     console.log(`Routing to testnet explorer port ${config['explorer-testnet'].http}`);
     proxy.web(req, res, { target: { host: 'localhost', port: config['explorer-testnet'].http }});
