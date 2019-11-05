0d3fa298366681e301ca4482e60244c95c2b2029
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2019-11-04 22:56:56 -0500

    ssl

--- index.js
+++ index.js
@@ -15,14 +15,36 @@ try {
 //
 // Create a proxy server with custom application logic
 //
-const proxy = httpProxy.createProxyServer({ ssl: ssl });
+const proxy = httpProxy.createProxyServer({});
 
 //
 // Create your custom server and just call `proxy.web()` to proxy
 // a web request to the target passed in the options
 // also you can use `proxy.ws()` to proxy a websockets request
 //
-const server = httpProxy.createServer( { target: { host: 'localhost', port: 9000 } });
+const server = http.createServer( ssl, function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.gi
+  console.log(req.headers);
+
+  let hostName = req.headers.host.split(':')[0];
+  let port = req.headers.host.split(':')[1];
+  if (hostName === 'explorer.blockchainfoundry.co') {
+    if (port !== config.wsPort) {
+      console.log(`Routing to explorer port ${config.explorer.http}`);
+      proxy.web(req, res, { target: 'localhost', port: config.explorer.http, ssl: ssl });
+    } else {
+
+    }
+  } else if (req.headers.host === 'explorer-testnet.blockchainfoundry.co') {
+    if (port !== config.wsPort) {
+      console.log(`Routing to testnet explorer port ${config['explorer-testnet'].http}`);
+      proxy.web(req, res, { port: config['explorer-testnet'].http, ssl });
+    } else {
+
+    }
+  }
+});
 
 //server.on('upgrade', function (req, socket, head) {
 //  proxy.ws(req, socket, head);
