7307c11335d5e95505c684242b01f2c111c6f8f4
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2019-11-04 23:00:44 -0500

    ssl

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const server = https.createServer( ssl, function(req, res) {
   if (hostName === 'explorer.blockchainfoundry.co') {
     if (port !== config.wsPort) {
       console.log(`Routing to explorer port ${config.explorer.http}`);
-      proxy.web(req, res, { target: 'localhost', port: config.explorer.http });
+      proxy.web(req, res, { target: { host: 'localhost', port: config.explorer.http }});
     } else {
 
     }
