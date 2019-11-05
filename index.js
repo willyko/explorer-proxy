a3a7fe9abee42ae112ed234245058c77f7dbc4ad
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2019-11-04 23:56:26 -0500

    ws

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ try {
 //
 // Create a proxy server with custom application logic
 //
-const proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer({
+  target: 'localhost',
+  ws: true
+});
+
+proxy.on('upgrade', function (req, socket, head) {
+  proxy.ws(req, socket, head);
+});
 
 //
 // Create your custom server and just call `proxy.web()` to proxy
@@ -36,10 +43,6 @@ const server = https.createServer( ssl, function(req, res) {
     console.log(`Routing to testnet explorer port ${config['explorer-testnet'].http}`);
     proxy.web(req, res, { target: { host: 'localhost', port: config['explorer-testnet'].http }});
   }
-});
-
-server.on('upgrade', function (req, socket, head) {
-  proxy.ws(req, socket, head);
 });
 
 console.log(`listening on port ${config.port}`);
