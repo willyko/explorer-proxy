c6650f0bcd150b9ab2fe8a9e2419c49916690642
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2020-02-10 23:15:17 -0500

    more flexible config and related refactor

--- index.js
+++ index.js
@@ -12,41 +12,48 @@ try {
   console.log('ssl error', e);
 }
 
+function parseHeaders(req) {
+  let host;
+  try {
+    host = req.headers.host.split(':')[0];
+    console.log('URL: ', req.url, host);
+  } catch (e) {
+    console.log('Error parsing:', req.headers);
+  }
+
+  return host;
+}
+
 //proxy
 const proxy = httpProxy.createProxyServer({});
 
 //main server
 const server = https.createServer( ssl, function(req, res) {
-  let host, ws;
-  try {
-    host = req.headers.host.split(':')[0];
-    ws = req.url.indexOf('zmq') !== -1;
-  } catch (e) {
-    console.log('Error parsing:', req.headers);
+  const host = parseHeaders(req);
+  const  hostEntry = Object.keys(config.hosts).find(hostKey => hostKey === host);
+
+  if (hostEntry && hostEntry.http) {
+    console.log(`>> Routing to request to  ${hostEntry.http}`);
+    proxy.web(req, res, {
+      target: {
+        host: 'localhost',
+        port: hostEntry.http
+      }
+    });
+  } else {
+    console.log(`No hostKey found for ${host} or host has no http entry`);
   }
-  console.log('URL: ', req.url, ws);
-  if (host === 'explorer.blockchainfoundry.co') {
-    console.log(`Routing to explorer port ${config.explorer.http}`);
-    proxy.web(req, res, {
-      target: {
-        host: 'localhost',
-        port: ws ? config.explorer.ws : config.explorer.http
-      }
-    });
-  } /*else if (host === 'explorer-testnet.blockchainfoundry.co') {
-    console.log(`Routing to testnet explorer port ${config['explorer-testnet'].http}`);
-    proxy.web(req, res, {
-      target: {
-        host: 'localhost',
-        port: config['explorer-testnet'].http
-      }
-    });
-  }*/
 });
 
 server.on('upgrade',function(req, socket, head){
-  console.log('upgrade websocket');
-  proxy.ws(req, socket, head, { target: { host: 'localhost', port: 9005, secure: true }});
+  const host = parseHeaders(req);
+  const  hostEntry = Object.keys(config.hosts).find(hostKey => hostKey === host);
+  if (hostEntry && hostEntry.ws) {
+    console.log('>> upgrade websocket ', host);
+    proxy.ws(req, socket, head, { target: { host: 'localhost', port: hostEntry.ws, secure: true }});
+  } else {
+    console.log(`No websocket hostKey found for ${host} or host has no ws entry`);
+  }
 });
 
 console.log(`listening on port ${config.port}`);
